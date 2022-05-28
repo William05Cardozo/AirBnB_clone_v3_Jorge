f08dbd7201fc143de6aca928dcd576b0b919a373
JorgeCal123/boom.git <git@github.com>
2022-05-28 20:23:17 +0000

    correction pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,10 +10,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(exception):
     """ closes the session """
     storage.close()
+
 
 if __name__ == "__main__":
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 
+
 @app_views.route("/status", strict_slashes=False)
 def status():
     """status"""
