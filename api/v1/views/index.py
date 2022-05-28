46d82190873ca8fdba30b122e353b837706dd7ce
JorgeCal123/boom.git <git@github.com>
2022-05-28 22:00:26 +0000

    pycodestyle in all py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 from models import storage
 
+
 @app_views.route("/status", strict_slashes=False)
 def status():
     """status"""
@@ -13,7 +14,6 @@ def status():
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
-    
     """methode stats"""
     dictionary = {"amenities": storage.count('Amenity'),
                   "cities": storage.count('City'),
