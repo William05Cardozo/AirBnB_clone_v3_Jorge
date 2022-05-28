fdf07642160296a9cc0c9acd3cf52361bdf57822
William05Cardozo <3784@holbertonschool.com>
2022-05-28 13:53:21 -0700

    Task 5: Add a EndPoint on Index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """index from views"""
 
+from models import *
+from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
 
@@ -8,3 +10,15 @@ from flask import Flask, Blueprint, jsonify
 def status():
     """status"""
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    dictt = {
+            "amenities": storage.count(storage.classes['Amenities']),
+            "cities": storage.count(storage.classes['City']),
+            "places": storage.count(storage.classes['Place']),
+            "revies": storage.count(storage.classes['Review']),
+            "states": storage.count(storage.classes['State']),
+            "users": storage.count(storage.classes['User'])
+            }
+    return jsonify(dictt)
