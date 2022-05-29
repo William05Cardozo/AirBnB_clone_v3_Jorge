5ec566669572b3d7985ddb273adb243d2a5c4445
William05Cardozo <3784@holbertonschool.com>
2022-05-28 21:08:37 -0700

    Task 6: Archives State_CorrectionCode

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -67,7 +67,7 @@ def put_states(state_id=None):
         abort(404)
     json_data = request.get_json()
     if not json_data:
-        return jsonify({'error':'Not a JSON'}), 400
+        return jsonify({'error': 'Not a JSON'}), 400
     for key, value in json_data.items():
         if key != "__class__":
             setattr(p_state, key, value)
