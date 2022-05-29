79b929027bb2dfb9eef2e571ca31ffd43fd2051a
William05Cardozo <3784@holbertonschool.com>
2022-05-29 16:35:58 -0700

    Index_CorrectionREpo

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,6 @@ from models import *
 from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
-from models import storage
 
 
 @app_views.route("/status", strict_slashes=False)
