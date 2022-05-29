86aaa88ab8a83bc2cd0a224ca6664c667920d417
JorgeCal123/boom.git <git@github.com>
2022-05-29 21:31:14 +0000

    correction file places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -56,11 +56,11 @@ def post_place(city_id=None):
         return jsonify({'error': 'Not a JSON'}), 400
     if "user_id" not in json_data.keys():
         return jsonify({'error': "Missing user_id"}), 400
+    if "name" not in json_data.keys():
+        return jsonify({'error': "Missing name"}), 400
     user = storage.get("User", json_data['user_id'])
     if user is None:
         abort(404)
-    if "name" not in json_data.keys():
-        return jsonify({'error': "Missing name"}), 400
     json_data['city_id'] = city_id
     place = Place(**json_data)
     storage.save()
