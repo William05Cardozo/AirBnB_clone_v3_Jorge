a05c63597390a860a6cfcf30a95420d9cae53683
JorgeCal123/boom.git <git@github.com>
2022-05-29 22:06:22 +0000

    correction file place_reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -79,8 +79,7 @@ def put_review(review_id=None):
     if not json_data:
         return jsonify({'error': 'Not a JSON'}), 400
     for key, value in json_data.items():
-        if key != "__class__" and key != "id" and key != "user_id" and\
-           key != "city_id" and key != "created_at" and key != "updated_at":
+        if key != "__class__":
             setattr(review, key, value)
     storage.save()
     return jsonify(review.to_dict()), 200
