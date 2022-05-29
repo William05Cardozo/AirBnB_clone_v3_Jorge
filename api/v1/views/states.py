90800bdf747cc4a81a690c28ae0c4fde3651e169
William05Cardozo <3784@holbertonschool.com>
2022-05-28 21:01:52 -0700

    Task 6: Archives State_CorrectionCode

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,30 +1,9 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """
 Create a new view for State objects that handles
 all default RESTFul API actions
+index from views
 """
-
-from models import storage
-from models.state import State
-from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, abort
-
-@app_views.route("/states", methods=['GET'], strict_slashes=False)
-@app_views.route("/states/<states_id", methods=['GET'], strict_slashes=False)
-def my_state(state_id=None):
-    list_state = []
-    if state_id == None:
-        for value in storage.all ("State").value():
-            list_state.append(value.to_dict())
-        return jsonify(list_state)
-    else:
-        state = storage.get("State", state_id)
-        if state == None:
-            abort(404)
-        return jsonify(state.to_dict())
-=======
-"""index from views"""
 
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, abort, request
